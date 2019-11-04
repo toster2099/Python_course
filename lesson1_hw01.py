0f39d812f7786432c0e79fc7d7c244ae192f497a
toster2099 <57312700+toster2099@users.noreply.github.com>
2019-11-04 10:11:55 +0200

    Add files via upload

---
+++ lesson1_hw01.py
@@ -0,0 +1,6 @@
+name = input("What is your name?:")
+print ("Hello," + name + "!")
+age = input("How old are you?:")
+print(age)
+print("Your name - " + name + ", " "your age - " + age)
+print("You are lucky Guy!")
